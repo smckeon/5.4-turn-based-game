f1099ffda665ca45b16ac3bf2f71523d35b8852e
Ryan Barron <ryanbarron243@gmail.com>
2017-02-09 16:31:37 -0500

    created constructors for character and hero.

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,4 +1,36 @@
 var $ = require ('jquery');
 var Handlebars = require ('handlebars')
 
-// Create 
+// Create
+
+
+//** Game Start
+//  Player chooses to be either hero or villain
+//  Turn one is always players Turn
+
+
+//  ** Hero/villian details
+// Set the hero and villains Health pools.
+//
+
+function Character(){
+  this.attack = function(){
+    // alert('BY MY BEARD')
+  };
+};
+
+
+function Hero(player){
+  this.health = player.health;
+};
+Hero.prototype = new Character();
+
+var player1 = new Hero({
+  health: 100,
+  attack: 10
+});
+player1.attack()
+console.log(player1.health)
+function Villain(){
+
+};
